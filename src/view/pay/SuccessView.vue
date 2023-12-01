3f8b8561c8816b7942269961bc356340f0040779
95Donguk <midgard95@nate.com>
2023-12-01 19:41:23 +0900

    :sparkles: feat: 결제 성공 후 결제 페이지가 나오고 5초 뒤에 홈으로 가게 구현

--- src/view/pay/SuccessView.vue
+++ src/view/pay/SuccessView.vue
@@ -76,6 +76,10 @@ export default {
   mounted() {
     // #TODO: 데이터베이스를 활용해서 FarmMoney를 업데이트 하기
     this.$store.commit("addFarmMoney");
+
+    setTimeout(() => {
+      this.$router.push("/home");
+    }, 5000);
   },
 };
 </script>
