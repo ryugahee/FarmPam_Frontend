61119b49cb80f96dfb85b80946536b4d6e5a0d52
95Donguk <midgard95@nate.com>
2023-12-04 20:36:17 +0900

    :bug: fix: 충돌 부분 해결

--- src/api/http.js
+++ src/api/http.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-    timeout: 10000,
-    baseURL: '/api',
-    headers:{
-        'content-type': 'application/json'
-    }
+  timeout: 10000,
+  baseURL: "/api",
+  headers: {
+    "content-type": "application/json",
+  },
 });
-
 
 export default instance;
