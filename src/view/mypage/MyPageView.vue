69806f32dff94daa488a67b651ed20554632c05a
95Donguk <midgard95@nate.com>
2023-12-14 16:06:32 +0900

    :sparkles: feat: 유저 팜머니 정보를 서버에서 가져오는 기능 추가

--- src/view/mypage/MyPageView.vue
+++ src/view/mypage/MyPageView.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-    <LOGO/>
+    <LOGO />
     <div class="user-info">
-      <div class="img-profile"><img src="../../../public/assets/img/person2.png" class="person-icon" alt=""/></div>
+      <div class="img-profile">
+        <img
+          src="../../../public/assets/img/person2.png"
+          class="person-icon"
+          alt=""
+        />
+      </div>
       <div class="user-nickname"><p>팜파미</p></div>
       <div>
         <button class="btn-user-info" @click="goUserInfo">프로필 수정</button>
@@ -16,30 +22,46 @@
           <div class="charge">150,000원</div>
         </div>
         <div class="pay-charge">
-          <div class="charge">충전하기</div>
-          <div><img src="../../../public/assets/img/left2.png" alt="" class="right"/></div>
+          <div class="charge" @click="goCharging" style="cursor: pointer">
+            충전하기
+          </div>
+          <div>
+            <img
+              src="../../../public/assets/img/left2.png"
+              alt=""
+              class="right"
+            />
+          </div>
         </div>
       </div>
     </div>
 
     <div class="container-list">
       <div class="list">
-        <div class="icons"><img src="../../../public/assets/img/shopping-bag1.png" alt=""/></div>
+        <div class="icons">
+          <img src="../../../public/assets/img/shopping-bag1.png" alt="" />
+        </div>
         <div @click="goPurchaseHistory">구매 내역</div>
       </div>
       <div class="list">
-        <div class="icons"><img src="../../../public/assets/img/sale1.png" alt=""/></div>
+        <div class="icons">
+          <img src="../../../public/assets/img/sale1.png" alt="" />
+        </div>
         <div @click="goSalesHistory">판매 내역</div>
       </div>
       <div class="list">
-        <div class="icons"><img src="../../../public/assets/img/receipt.png" alt=""/></div>
+        <div class="icons">
+          <img src="../../../public/assets/img/receipt.png" alt="" />
+        </div>
         <div>참여 이력</div>
       </div>
       <div class="list">
-        <div class="icons"><img src="../../../public/assets/img/inquiry.png" alt=""/></div>
+        <div class="icons">
+          <img src="../../../public/assets/img/inquiry.png" alt="" />
+        </div>
         <div>문의 하기</div>
       </div>
-      <NavComponent/>
+      <NavComponent />
     </div>
   </div>
 </template>
@@ -54,31 +76,31 @@ import AuctionSaleHistoryView from "@/view/mypage/AuctionSaleHistoryView.vue";
 export default {
   computed: {
     SalesHistory() {
-      return AuctionSaleHistoryView
+      return AuctionSaleHistoryView;
     },
     PurchaseHistory() {
-      return AuctionPurchaseHistoryView
-    }
+      return AuctionPurchaseHistoryView;
+    },
   },
-  components: {NavComponent, LOGO},
+  components: { NavComponent, LOGO },
 
   methods: {
     goUserInfo() {
-      router.push({path: "/user/info"});
+      router.push({ path: "/user/info" });
     },
     goPurchaseHistory() {
-      router.push({path: "/user/history/purchase"});
+      router.push({ path: "/user/history/purchase" });
     },
     goSalesHistory() {
-      router.push({path: "/user/history/sales"});
+      router.push({ path: "/user/history/sales" });
     },
-  }
-
-}
+    goCharging() {
+      router.push({ path: "/charging" });
+    },
+  },
+};
 </script>
 
 <style scoped>
 @import "../../../public/assets/css/mypage.css";
-
-
 </style>
