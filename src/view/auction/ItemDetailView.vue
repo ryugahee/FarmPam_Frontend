56c9727e13c377c750d36e16c4e6d261aeae2a9e
KKchan1119 <kkchan1119@gmail.com>
2023-12-14 19:23:52 +0900

    feature: Bid Finish Proccessing

--- src/view/auction/ItemDetailView.vue
+++ src/view/auction/ItemDetailView.vue
@@ -140,7 +140,7 @@ export default {
   methods: {
     connect() {
       this.receiveBidList();
-      this.userName = this.$store.state.user.id;
+      this.userName = localStorage.getItem("username");
       //소켓 연결
       const serverURL = "http://localhost:8080/bid";
       let socket = new SocketJS(serverURL);
@@ -206,6 +206,7 @@ export default {
         .post("/bid-list", JSON.stringify(msg), {})
         .then((res) => {
           this.receiveList = res.data;
+          console.log("receiveData"+this.receiveList.at(0))
           this.currentPrice = this.receiveList.at(-1);
         })
         .catch((err) => {
